599a3383fff550caa476a7d5e820c277cef4aafb
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-08-21 10:25:52 +0200

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,1 +1,25 @@
+module.exports = function () {
+  this.add = function (a, b) {
+    return a + b;
+  };
 
+  this.multiply = function (a, b) {
+    return a * b;
+  };
+  
+  this.subtract = function (a, b) {
+    return a - b;
+  };
+  
+  this.divide = function (a, b) {
+    return a / b;
+  };
+  
+  this.fibonacci = function (length) {
+    var sequence = [0, 1];
+    for (var i = 2; i <= length; ++i) {
+      sequence[i] = sequence[i - 1] + sequence[i - 2];
+    }
+    return sequence;
+  };
+};
