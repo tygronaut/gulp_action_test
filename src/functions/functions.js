ed228a10ddd812b526ab979dd83692234e0e8b2a
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-08-31 22:54:06 +0200

    Update functions.js

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -1,20 +1,20 @@
-const add = function (a, b) {
+function add(a, b) {
   return a + b;
 };
 
-const multiply = function (a, b) {
+function multiply(a, b) {
   return a * b;
 };
 
-const subtract = function(a, b) {
+function subtract(a, b) {
   return a - b;
 };
 
-const divide = function(a, b) {
+function divide(a, b) {
   return a / b;
 };
 
-const fibonacci = function (length) {
+function fibonacci(length) {
   var sequence = [0, 1];
   for (var i = 2; i <= length; ++i) {
     sequence[i] = sequence[i - 1] + sequence[i - 2];
