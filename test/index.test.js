2c7a17868290c59a73a3dc03663e422a6dc1a256
tygronaut <141247889+tygronaut@users.noreply.github.com>
2025-09-02 21:21:01 +0000

    remove functions prefix

--- test/index.test.js
+++ test/index.test.js
@@ -4,7 +4,7 @@ describe('functions', () => {
   describe('add', () => {
     it('adds two numbers together', () => {
       
-      const result = functions.add(6, 2);
+      const result = add(6, 2);
       expect(result).toBe(8);
     });
   });
@@ -12,7 +12,7 @@ describe('functions', () => {
   describe('multiply', () => {
     it('multiply two numbers', () => {
       
-      const result = functions.multiply(3, 3);
+      const result = multiply(3, 3);
       expect(result).toBe(9);
     });
   });
@@ -20,7 +20,7 @@ describe('functions', () => {
   describe('multiply', () => {
     it('multiply 4 times 4', () => {
       
-      const result = functions.multiply(4, 4);
+      const result = multiply(4, 4);
       expect(result).toBe(16);
     });
   });
@@ -28,7 +28,7 @@ describe('functions', () => {
     describe('multiply', () => {
     it('multiply 5 times 6', () => {
       
-      const result = functions.multiply(5, 6);
+      const result = multiply(5, 6);
       expect(result).toBe(30);
     });
   });
@@ -36,7 +36,7 @@ describe('functions', () => {
   describe('fibonacci', () => {
     it('generates a valid fibonacci sequence', () => {
       
-      const result = functions.fibonacci(12);
+      const result = fibonacci(12);
       expect(result[12]).toBe(144);
     });
   });
